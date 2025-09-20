7e16036dfe07b5e230d14ef986d283549e712166
Oussama <me.ouss.am@outlook.de>
2025-09-20 12:31:05 +0200

    solved 4 tasks with k=500

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import json
+from .src.preprocessing import run
+
+def main():
+    run()
+
+if if __name__ == "__main__":
+    main()
+    
+'data/arc-agi_evaluation_challenges.json'
+'data/arc-agi_evaluation_solutions.json'
+'data/arc-agi_test_challenges.json'
+
